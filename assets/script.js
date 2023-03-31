b87e305dc5532e5e84f74e9a38e27945aecee4d6
Gaetan Callens <c.05contact.ag@gmail.com>
2023-03-31 16:55:29 +0200

    Etape 1

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,25 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let index=0
+const image= document.getElementsByClassName("banner-img")[0]
+const goNextButton = document.getElementById("arrow_right")
+const goBackButton = document.getElementById("arrow_left")
+
+const goNext = () => {
+    index=index+1
+    image.src="./assets/images/slideshow/"+ slides[index].image
+    //tag.innerHTML=//
+}
+
+const goBack = () => {
+    index=index-1
+    console.log(index)
+    image.src="./assets/images/slideshow/"+ slides[index].image
+}
+
+goNextButton.addEventListener("click", goNext, false)
+
+goBackButton.addEventListener("click", goBack, false)
+
