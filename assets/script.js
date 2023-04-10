56c4ca4a3f93845c4c0c1997c36930cc21dba115
Gaetan Callens <c.05contact.ag@gmail.com>
2023-04-10 12:28:31 +0200

    bullet point & banner

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ const slides = [
 	{
 		"image":"slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+
 	},
 	{
 		"image":"slide3.jpg",
@@ -17,24 +18,65 @@ const slides = [
 	}
 ]
 
-let index=0
-const image= document.getElementsByClassName("banner-img")[0]
+const createBulletPoint = (index) => {
+    const bulletPointContainer = document.getElementsByClassName("dots")[0]
+    while (bulletPointContainer.firstChild) {
+        bulletPointContainer.firstChild.remove()
+    }
+    for (let i = 0; i < slides.length; i++ ) {
+        const bulletPoint = document.createElement("div")
+        if (i === index) {
+            bulletPoint.className="dot dot_selected"
+        } else {
+            bulletPoint.className="dot"
+        }
+        bulletPointContainer.appendChild(bulletPoint)
+    }
+}
+
+createBulletPoint(0)
+
+let index = 0
+const image = document.getElementsByClassName("banner-img")[0]
+const tag = document.getElementById("tagLine");
 const goNextButton = document.getElementById("arrow_right")
 const goBackButton = document.getElementById("arrow_left")
 
 const goNext = () => {
-    index=index+1
-    image.src="./assets/images/slideshow/"+ slides[index].image
-    //tag.innerHTML=//
+    if (index < slides.length - 1) {
+        console.log(index)
+        index = index + 1
+        createBulletPoint(index)
+        image.src="./assets/images/slideshow/"+ slides[index].image
+        tag.innerHTML=slides[index].tagLine
+    } else {
+        console.log(index)
+        index = 0
+        createBulletPoint(index)
+        image.src="./assets/images/slideshow/"+ slides[index].image
+        tag.innerHTML=slides[index].tagLine
+    }
 }
 
 const goBack = () => {
-    index=index-1
-    console.log(index)
-    image.src="./assets/images/slideshow/"+ slides[index].image
+    if (index > 0) {
+        console.log(index)
+        index = index - 1
+        createBulletPoint(index)
+        image.src="./assets/images/slideshow/"+ slides[index].image
+        tag.innerHTML=slides[index].tagLine
+    } else {
+        console.log(index)
+        index = slides.length - 1
+        createBulletPoint(index)
+        image.src="./assets/images/slideshow/"+ slides[index].image
+        tag.innerHTML=slides[index].tagLine
+    }
 }
 
 goNextButton.addEventListener("click", goNext, false)
-
 goBackButton.addEventListener("click", goBack, false)
 
+
+
+// Message //
